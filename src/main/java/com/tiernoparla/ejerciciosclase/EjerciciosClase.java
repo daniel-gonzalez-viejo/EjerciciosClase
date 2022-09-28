3de16c2445ac721f1d6419c8e1aa5099a810487a
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-09-28 18:31:35 +0200

    actualizo cosas clase

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/EjerciciosClase.java
@@ -0,0 +1,51 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class EjerciciosClase {
+
+    public static void main(String[] args) {
+       
+        int n = 0;
+        while (n < 11){
+            if (n%2 == 0){
+            System.out.println(n);
+             } //if
+            n++;
+
+        } //while
+            } //main        
+        
+}
+
+ 
+     //  int n = 0;
+      //  while(n < 11){
+        //    System.out.println(n);
+         //   n++;
+      //  } //while
+        
+     //main
+        
+        //  for (int i= 1; i < 11; i++){
+            
+    //        if(i%2 != 0){
+      //          System.out.println(i);
+        //    } // if
+            
+         //for
+        
+        
+        // printear numeros pares del 1 al 10
+       //  int n = 0;
+       //  System.out.println(n);
+       //  n = 1;
+       //  System.out.println(n);
+        //////////////////////
+       //  for(int i = 1; i < 11; i++){
+       //  System.out.println(i);   
+            
+        
+        
+        
+     //main
+ //RepoEjercicios
+
