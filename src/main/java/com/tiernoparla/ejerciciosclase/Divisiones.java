41b51aa265f05ce460e293f34102a76d18c43067
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-09-29 17:22:49 +0200

    añado ejercicio de divisiones

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/Divisiones.java
@@ -0,0 +1,18 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class Divisiones {
+    public static void main(String[] args) {
+        
+        int n = 33;
+        int d = 5;
+        int r = n;
+        int q = 0;
+            while(r>=d){
+                r = r - d;
+                q++; 
+            } //while
+                System.out.println("El resultado de r es: " + n);
+                System.out.println("El resultado de q es: " + q);
+        
+    } //main
+} //Divisiones
