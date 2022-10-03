9c7f928287f16af083fa308f862228e37647bbfd
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-03 21:43:12 +0200

    actualizo bien ejemplo de arrays fibonacci

--- src/main/java/com/tiernoparla/ejerciciosclase/FibonacciArray.java
+++ src/main/java/com/tiernoparla/ejerciciosclase/FibonacciArray.java
@@ -7,20 +7,20 @@ public class FibonacciArray {
         // 2º inicializo el array con {x1= 1, x2 = 1, Fib = 2}
         // 3º hago el for --> pero sustituyendo 
         
-        int [] ejemplos = {1, 1};
-        
-        int Fib;
+        int [] ejemplos = {1, 1, 0, 7};
         
         
+        for (int i = 3; i <= ejemplos[3]; i++) {
+            ejemplos[2] = ejemplos[1] + ejemplos[0];
+            ejemplos[0] = ejemplos[1];
+            ejemplos[1] = ejemplos[2];
+            System.out.println(ejemplos[2]);
+            
+        } //for
         
         
         
         
-        for (int i = 0; i < ejemplos.length; i++){ // Mientras que i sea menor que la lngitud maxima del array, hará lo indicado
-        System.out.println(ejemplos[2]);
         
-        ejemplos [0] = ejemplos [1];
-        ejemplos [1] = int Fib;
-        }
-    } // main
-} //all
+        } //main
+    } // all
