7f6ee3de26cf4e11785cf2791787f6793ffb727d
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-10 18:08:19 +0200

    añado metodo de contar vocales con string

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/ContarVocalesString.java
@@ -0,0 +1,18 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class ContarVocalesString {
+public static void main(String[] args){
+        
+        String srt = "magico";
+        
+        int Vocales = 0;
+        
+        for (int i = 0; i < srt.length(); i++) {
+        char loc = srt.charAt(i);
+        if (loc == 'a' || loc == 'e' || loc == 'i'|| loc == 'o'|| loc == 'u')
+        Vocales++;
+        } //if
+        System.out.println(Vocales);
+        } //main
+} //all
+
