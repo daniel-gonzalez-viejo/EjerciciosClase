e3b7f65696202ba2671a10e78d6941eaf73ace55
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 00:06:53 +0200

    añado ejemplo de password

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/Password.java
@@ -0,0 +1,22 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class Password {
+    public static void main(String[] args) {
+        
+        String str = "magicamente10";
+        // NO ESTÁ ACABADO
+        
+        if (str.length()<10){
+            System.out.println("La contraseña es correcta1");
+        }//if
+        else if (str.charAt([ASCII)]){
+            System.out.println("La contraseña es correcta2");
+        } // else if
+        else if (str.charAt("[0-9]")){
+            System.out.println("La contraseña es correcta3");
+        } // else if
+        else{
+            System.out.println("La contraseña no es correcta");
+        }
+    } //main
+} //all
