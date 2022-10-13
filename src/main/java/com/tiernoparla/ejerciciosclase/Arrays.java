770b4198a38a115e058b417ad904aca6766c8430
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 00:11:29 +0200

    actualizado ejercicio de arrays

--- src/main/java/com/tiernoparla/ejerciciosclase/Arrays.java
+++ src/main/java/com/tiernoparla/ejerciciosclase/Arrays.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tiernoparla.ejerciciosclase;
 
-/**
- *
- * @author Dani
- */
 public class Arrays {
-    
-}
+    public static void main(String[] args) {
+        
+        String [] palabras = {"hola", "adios"};
+        
+        String [] palabras2 = new String[23]; // Para fijar cantidad de memoria en el Array
+        
+        System.out.println(palabras2[4]);
+        
+        
+        
+        
+        System.out.println(palabras[0]);
+        System.out.println(palabras[1]);
+        
+        palabras [0] = "algo aqui";
+        System.out.println(palabras[0]);
+        
+        for (int i = 0; i < palabras.length; i++){ // Mientras que i sea menor que la lngitud maxima del array, hará lo indicado
+            System.out.println(palabras[i]);
+        }
+    } //main
+} //Arrays
