d34b2c6e2a768e7e40a177c0acace20aa3490d9a
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 00:10:38 +0200

    añado ejemplo 1 uso de strings

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/Strings.java
@@ -0,0 +1,8 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class Strings {
+    public static void main(String[] args) {
+        String let = "abcdfgh";
+        System.out.println(let.charAt(let.length()/2));
+    } //main
+} //ALL
