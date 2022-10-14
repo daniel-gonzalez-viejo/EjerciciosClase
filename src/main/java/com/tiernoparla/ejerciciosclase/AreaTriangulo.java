1e1b96191019631bbd5789e8ffe7e2b532c5e7a9
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 19:46:18 +0200

    añado ejemplo de como calcular el area de un triangulo solo con los lados

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/AreaTriangulo.java
@@ -0,0 +1,20 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class AreaTriangulo {
+    public static void main(String[] args) {
+        int cat1 = 10; //Cateto 1
+        int cat2 = 10; //Cateto 2
+        int base = 18; //Base
+        int x = base/2;
+        int h = (cat1^2) + (x^2); //La altura
+        double d = Math.sqrt(h); //Para sacar la raiz cuadrada de h
+
+        
+        
+        double area = ((base * d) / 2);
+        
+        
+        
+        System.out.println(area);
+    } //main
+} //all
