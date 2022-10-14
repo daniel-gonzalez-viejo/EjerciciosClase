51be278ac0af9dbcc1e813718f5c110b8e0e5dce
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 19:46:51 +0200

    añado ejemplo de como sacar matriz aleatoria entre 0 y 1

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/Matriz.java
@@ -0,0 +1,15 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class Matriz {
+    public static void main(String[] args) {
+        int matriz [][] = new int [5][5];
+        
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                matriz[x][y] = (int) (Math.random()*2);
+                System.out.print(matriz [x][y]);
+            } //for
+            System.out.println();
+        }//for  
+    } //main
+} //all
