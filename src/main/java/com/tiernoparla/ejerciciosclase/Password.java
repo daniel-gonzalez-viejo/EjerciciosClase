0846d4ab4c833975b49864a25bb1e3dc5b45f2c7
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-14 19:45:16 +0200

    aun no resuelto

--- src/main/java/com/tiernoparla/ejerciciosclase/Password.java
+++ src/main/java/com/tiernoparla/ejerciciosclase/Password.java
@@ -4,19 +4,20 @@ public class Password {
     public static void main(String[] args) {
         
         String str = "magicamente10";
+        int lon = 10;
         // NO ESTÁ ACABADO
-        
-        if (str.length()<10){
+        if (str.length() < lon && str.substring(1, 10).matches("[a-z]*") && str.substring(1, 10).matches("[0-9]*")){
             System.out.println("La contraseña es correcta1");
         }//if
-        else if (str.charAt([ASCII)]){
+        /*else if (str.substring(0, 3).matches("[A-Z]*")){
             System.out.println("La contraseña es correcta2");
         } // else if
-        else if (str.charAt("[0-9]")){
+        else if (str.substring(0, 3).matches("[0-9]*")){
             System.out.println("La contraseña es correcta3");
         } // else if
+        */
         else{
             System.out.println("La contraseña no es correcta");
-        }
+        } //else
     } //main
 } //all
