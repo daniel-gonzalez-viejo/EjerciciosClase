96acd8de55f3b4a5b980e3ad09f270ff0999bd65
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-17 19:33:19 +0200

    añado segundo metodo matriz

--- src/main/java/com/tiernoparla/ejerciciosclase/Matriz.java
+++ src/main/java/com/tiernoparla/ejerciciosclase/Matriz.java
@@ -2,14 +2,24 @@ package com.tiernoparla.ejerciciosclase;
 
 public class Matriz {
     public static void main(String[] args) {
-        int matriz [][] = new int [5][5];
+        
+        int N = 5;
+        int matriz [][] = new int [N][N];
+        
+        //METODO 1 PRINTEAR NUMEROS
         
         for (int x = 0; x < matriz.length; x++) {
             for (int y = 0; y < matriz[x].length; y++) {
-                matriz[x][y] = (int) (Math.random()*3);
+                matriz[x][y] = (int)(Math.random()*2);
                 System.out.print(matriz [x][y]);
             } //for
             System.out.println();
+            
+        //METODO 2 PRINTEAR NUMEROS
+        
+        for (int[] ara: matriz){
+            System.out.println(Arrays.toString(ara));
+        }//for
         }//for  
     } //main
 } //all
