93c0f4eeb9917909abbc8f678f5d831622ccf5af
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-10-18 18:45:47 +0200

    actualizo matriz

--- src/main/java/com/tiernoparla/ejerciciosclase/Matriz.java
+++ src/main/java/com/tiernoparla/ejerciciosclase/Matriz.java
@@ -10,16 +10,16 @@ public class Matriz {
         
         for (int x = 0; x < matriz.length; x++) {
             for (int y = 0; y < matriz[x].length; y++) {
-                matriz[x][y] = (int)(Math.random()*2);
-                System.out.print(matriz [x][y]);
-            } //for
-            System.out.println();
+                matriz[x][y] = (int)(Math.random()*2); //Se sustituyen las variables "x" e "y" por aleatorios entre "0" y "1"
+                System.out.print(matriz [x][y]); 
+        } //for
+        System.out.println();
             
-        //METODO 2 PRINTEAR NUMEROS
+        //METODO 2 PRINTEAR NUMEROS (AUNQUE ME ESTÁ FALLANDO)
         
-        for (int[] ara: matriz){
-            System.out.println(Arrays.toString(ara));
-        }//for
+        //for (int[] ara: matriz){
+        //    System.out.println(Arrays.toString(ara));
+        // }//for
         }//for  
     } //main
 } //all
