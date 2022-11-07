487e8f323de6ce71a68a2e93a24d2211ca71b4c4
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-11-07 04:28:57 +0100

    Lo tengo que terminar cuando me acuerde

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/NumerosPrimosGemelos.java
@@ -0,0 +1,17 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class NumerosPrimosGemelos {
+    
+    public static void main(String[] args) {
+        
+    int p = 39;
+
+    
+        for (int i = 2; i < p; i++) {
+            if (p%i == 0) {
+                System.out.println(i);
+            }
+        }
+        
+    }//main
+}//all
