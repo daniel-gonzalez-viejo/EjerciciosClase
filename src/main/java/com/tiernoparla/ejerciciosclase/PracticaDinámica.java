66ae07a527c5bd82e45535556120c77c9bc7b16b
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2022-11-07 04:29:49 +0100

    No recuerdo si la he terminado

---
+++ src/main/java/com/tiernoparla/ejerciciosclase/PracticaDinámica.java
@@ -0,0 +1,14 @@
+package com.tiernoparla.ejerciciosclase;
+
+public class PracticaDinámica {
+    
+    int edad = 23;
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Dani";
+
+        System.out.printf("Mi nombre es %d.\n", nombre);
+        
+    }
+}
