417309e86d48431fe18c8f97d38924ea60b7c674
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 00:15:09 +0100

    añado un ejemplo para ver si comitea bien en otra

--- src/main/java/practicacom/practica/programacion/practicaProgramacion.java
+++ src/main/java/practicacom/practica/programacion/practicaProgramacion.java
@@ -1,5 +1,7 @@
 package practicacom.practica.programacion;
 
 public class practicaProgramacion {
-    
+    public static void main(String[] args) {
+        System.out.println("es una prueba");
+    }
 }
