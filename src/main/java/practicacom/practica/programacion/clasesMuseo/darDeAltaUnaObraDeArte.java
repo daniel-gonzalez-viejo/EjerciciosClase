8f8a39a6e4da97efd0c223c8fe3caafc75258315
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 00:37:25 +0100

    añado atributos caracteristicos de una obra

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -0,0 +1,17 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class darDeAltaUnaObraDeArte {
+    private int ID;
+    private String Tipo;
+    private String Nombre;
+    private String Autor;
+    private double Precio;
+    private int Altura;
+    private int Peso;
+    private String Material;
+    private String Tecnica;
+    private int Piezas;
+    private String Descripción;
+
+    
+}
