88357daccfd2d1be9f74a5c4c7776417df1f53f0
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 00:17:45 +0100

    es un segundo ejemplo

--- src/main/java/practicacom/practica/programacion/practicaProgramacion.java
+++ src/main/java/practicacom/practica/programacion/practicaProgramacion.java
@@ -3,5 +3,6 @@ package practicacom.practica.programacion;
 public class practicaProgramacion {
     public static void main(String[] args) {
         System.out.println("es una prueba");
+        System.out.println("es una prueba 2");
     }
 }
