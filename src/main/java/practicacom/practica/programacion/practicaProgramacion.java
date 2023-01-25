ebfe10cc4e381d476816f19d8d5e10ddc755f371
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 01:33:35 +0100

    se añade funcionalidad al switch para visualizar

--- src/main/java/practicacom/practica/programacion/practicaProgramacion.java
+++ src/main/java/practicacom/practica/programacion/practicaProgramacion.java
@@ -10,18 +10,17 @@ public class practicaProgramacion {
 
         darDeAltaUnaObraDeArte art = new darDeAltaUnaObraDeArte(1, "Cuadro", "Cuadro de mariposas", "Van Gogh", 
         4, 5, 2, null, "oleo", 1, "Un cuadro mu feo");
+        art.setAutor("Van gogh");
 
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Qué quieres hacer");
         System.out.println("1. Visualizar obras de arte");
-        int n = sc.nextInt();
+        String n = sc.nextLine();
 
         switch (n){
-            case 1: {
-                System.out.println("soy tonot");
-            }
-            case 2: {
-                System.out.println("no soy tonot");
+            case "Ver cuadros": {
+                System.out.println(art.getCabeceros());
+                System.out.println(art.toString());
             }
         }
     }
