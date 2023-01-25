e0a75d628d3e08ba5edd1ba723c88ba79d2992ac
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 01:05:18 +0100

    añado atributos

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -0,0 +1,18 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class darDeAltaUnaObraDeArte {
+    private int ID;
+    private String Tipo;
+    private String Nombre;
+    private String Autor;
+    private double Precio;
+    private int Altura;
+    private int Peso;
+    private String Material;
+    private String Tecnica;
+    private int Piezas;
+    private String Descripción;
+    
+
+    
+}
