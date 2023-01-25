5a32c43076a83c53a41574a9617c62604ce48476
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 01:13:53 +0100

    se añade scanner a la practica

--- src/main/java/practicacom/practica/programacion/practicaProgramacion.java
+++ src/main/java/practicacom/practica/programacion/practicaProgramacion.java
@@ -1,8 +1,25 @@
 package practicacom.practica.programacion;
 
+import practicacom.practica.programacion.clasesMuseo.darDeAltaUnaObraDeArte;
+import java.util.Scanner;
+
 public class practicaProgramacion {
     public static void main(String[] args) {
-        System.out.println("es una prueba");
-        System.out.println("es una prueba 2");
+
+
+
+        darDeAltaUnaObraDeArte art = new darDeAltaUnaObraDeArte(1, "Cuadro", "Cuadro de mariposas", "Van Gogh", 
+        4, 5, 2, null, "oleo", 1, "Un cuadro mu feo");
+
+        int n = 2;
+
+        switch (n){
+            case 1: {
+                System.out.println("soy tonot");
+            }
+            case 2: {
+                System.out.println("no soy tonot");
+            }
+        }
     }
 }
