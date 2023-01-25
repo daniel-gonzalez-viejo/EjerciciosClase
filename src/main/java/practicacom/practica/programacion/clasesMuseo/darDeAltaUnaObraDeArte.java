3155ad64eb25f90e037ae802dd151af33d8743e0
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-25 01:07:22 +0100

    añadidos getters y setters

--- src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -12,7 +12,7 @@ public class darDeAltaUnaObraDeArte {
     private String Tecnica;
     private int Piezas;
     private String Descripción;
-    
+
     public darDeAltaUnaObraDeArte(int iD, String tipo, String nombre, String autor, double precio, int altura, int peso,
             String material, String tecnica, int piezas, String descripción) {
         ID = iD;
@@ -27,6 +27,95 @@ public class darDeAltaUnaObraDeArte {
         Piezas = piezas;
         Descripción = descripción;
     }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double precio) {
+        Precio = precio;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int altura) {
+        Altura = altura;
+    }
+
+    public int getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(int peso) {
+        Peso = peso;
+    }
+
+    public String getMaterial() {
+        return Material;
+    }
+
+    public void setMaterial(String material) {
+        Material = material;
+    }
+
+    public String getTecnica() {
+        return Tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        Tecnica = tecnica;
+    }
+
+    public int getPiezas() {
+        return Piezas;
+    }
+
+    public void setPiezas(int piezas) {
+        Piezas = piezas;
+    }
+
+    public String getDescripción() {
+        return Descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        Descripción = descripción;
+    }
+    
     
 
     
