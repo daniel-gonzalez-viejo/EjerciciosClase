31c021ebb65db95f0539b66786e25495ddcf73cb
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-26 18:10:27 +0100

    añado funcionalidad del menu

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/abrirMenu.java
@@ -0,0 +1,21 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class abrirMenu {
+    private int inserción;
+
+    public abrirMenu(int inserción) {
+        this.inserción = inserción;
+    }
+
+    public int insertarMenu(int inserción) {
+
+        System.out.println("¿Qué quieres hacer?");
+
+       switch (inserción) {
+          case 1: {
+            
+          }
+       } 
+    }
+    
+}
--- src/main/java/practicacom/practica/programacion/practicaProgramacion.java
+++ src/main/java/practicacom/practica/programacion/practicaProgramacion.java
@@ -8,19 +8,22 @@ public class practicaProgramacion {
 
 
 
-        darDeAltaUnaObraDeArte art = new darDeAltaUnaObraDeArte(1, "Cuadro", "Cuadro de mariposas", "Van Gogh", 
-        4, 5, 2, null, "oleo", 1, "Un cuadro mu feo");
-        art.setAutor("Van gogh");
+        darDeAltaUnaObraDeArte art = new darDeAltaUnaObraDeArte(001, "Cuadro", "Guernica", "P.Picasso", 
+        1000, 5, 2, null, "Óleo", 1, "Un cuadro mu feo");
+        darDeAltaUnaObraDeArte arte = new darDeAltaUnaObraDeArte(001, "Cuadro", "Guernica", "P.Picasso", 
+        1000, 5, 2, null, "Óleo", 1, "Un cuadro mu feo");
+        darDeAltaUnaObraDeArte arto = new darDeAltaUnaObraDeArte(001, "Cuadro", "Guernica", "P.Picasso", 
+        1000, 5, 2, null, "Óleo", 1, "Un cuadro mu feo");
 
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Qué quieres hacer");
-        System.out.println("1. Visualizar obras de arte");
+        System.out.println("1. Ver cuadros");
         String n = sc.nextLine();
 
         switch (n){
             case "Ver cuadros": {
-                System.out.println(art.getCabeceros());
-                System.out.println(art.toString());
+                System.out.println(art.getID());
+
             }
         }
     }
