5a59663f15b955ce948a17828972dcbe025e924a
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-01-30 18:07:20 +0100

    creo la clase verObras

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/verObras.java
@@ -0,0 +1,13 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class verObras extends darDeAltaUnaObraDeArte {
+
+    public verObras(String iD, String tipo, String nombre, String autor, double precio, double altura, double peso,
+            String material, String tecnica, int piezas, String descripción) {
+        super(iD, tipo, nombre, autor, precio, altura, peso, material, tecnica, piezas, descripción);
+    }
+    
+    
+
+
+}
