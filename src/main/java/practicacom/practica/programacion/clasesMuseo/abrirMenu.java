36c6dc18fc7bf2616f9c6ae49b962443ffdc26a6
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 02:43:00 +0100

    se añaden objetos a la clase, y se inserta el metodo verObra

--- src/main/java/practicacom/practica/programacion/clasesMuseo/abrirMenu.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/abrirMenu.java
@@ -1,33 +1,42 @@
 package practicacom.practica.programacion.clasesMuseo;
 import java.util.Scanner;
+import java.util.Vector;
 
-public class abrirMenu{
-    private int opcionMenu;
-    //protected verObras objeto[] = new verObras[5];
+import practicacom.practica.programacion.clasesMuseo.obras;
 
-    final private String OPCIONES = "¿Qué quieres hacer?";
-    final private String OPCION_1 = "1. Ver cuadros";
-    final private String OPCION_2 = "2. Dar de alta";
-    final private String OPCION_3 = "3. Modificar los datos de una de las obras";
-    final private String OPCION_4 = "4. Visualizar los datos de una de las obras de arte expuestas";
-    final private String OPCION_5 = "5. Obtener el precio de venta de una de las obras de arte expuestas";
-    final private String OPCION_6 = "6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional";
-    final private String SALIR = "7. Salir";
+public class abrirMenu {
+    public static void main(String[] args) {
+        
+    int opcionMenu;
+
+    final String OPCIONES = "¿Qué quieres hacer?";
+    final String OPCION_1 = "1. Ver cuadros";
+    final String OPCION_2 = "2. Dar de alta";
+    final String OPCION_3 = "3. Modificar los datos de una de las obras";
+    final String OPCION_4 = "4. Visualizar los datos de una de las obras de arte expuestas";
+    final String OPCION_5 = "5. Obtener el precio de venta de una de las obras de arte expuestas";
+    final String OPCION_6 = "6. Imprimir una etiqueta para clasificar una de las obras expuestas y dar información adicional";
+    final String SALIR = "7. Salir";
     
     boolean interruptor = false;
 
     Scanner sc = new Scanner(System.in);
 
-    public abrirMenu(int opcionMenu, boolean interruptor) {
-        this.opcionMenu = opcionMenu;
-        this.interruptor = interruptor;
-    }
+    obras P1 = new obras("001" , "Pintura", "Guernica", "P.Picasso" , 1000 , 5 , 2 ,   "null" ,   "Óleo" , 5 , "Cuadro de la guerra civil");
+    obras P2 = new obras("002" , "Pintura", "La Vie" , "P.Picasso" , 200 , 5 , 1 , "null" , "Óleo" , 5 , "Óleo");
+    obras P3 = new obras("003" , "Pintura", "El Sueño" , "P.Picasso" , 300 , 5 , 1.3 , "null" ,   "Óleo" , 5 , "Óleo");
+    obras P4 = new obras("004" , "Pintura", "Retrato.D Maar" , "P.Picasso" , 400 , 5 , 1 , "null" , "Óleo" , 5 , "Óleo");
+    obras P5 = new obras("005" , "Escultura", "El piel roja" , "U.Chueca" , 350 , 5 , 1 ,  "acero" , "null" ,  5 , "Escultura");
+    
+    verObras Pa = new verObras(10);
+    Pa.agregar(P1);
+    Pa.agregar(P2);
+    Pa.agregar(P3);
+    Pa.agregar(P4);
+    Pa.agregar(P5);
+    
 
-    public void Cabecera() {
-        System.out.println("-------------------------------------------------------------------------------------------------------");
-    }
 
-    public void insertarMenu() {
 
         while (!interruptor) {
             System.out.println("-------------------------------------------------------------------------------------------------------");
@@ -41,22 +50,21 @@ public class abrirMenu{
             System.out.println(SALIR);
             System.out.println("-------------------------------------------------------------------------------------------------------");
             opcionMenu = sc.nextInt();
+            
 
         switch (opcionMenu){
             case 1: {
-                verObras ob = new verObras();
-                ob.verLasObras();
+                System.out.println(P1.Cabeceros());
+                System.out.println(Pa.toStrin());
+                break;
             }  
-            /*
+            
             case 2: {
-                darDeAltaUnaObraDeArte dar = new darDeAltaUnaObraDeArte();
-                //darDeAltaUnaObraDeArte objeto2[]=new darDeAltaUnaObraDeArte[6];
-                //for(int i = 0; i < objeto.length; i++) {
-                //    objeto2[i] = objeto[i];
-                //}
-                // objeto[5] = dar.darDeAlta();
-                // break;
-            } 
+                obras P6 = new obras("006" , "Pintura", "Guernica", "P.Picasso" , 1000 , 5 , 2 ,   "null" ,   "Óleo" , 5 , "Cuadro de la guerra civil");
+                Pa.agregar(P6);
+                //System.out.println(P1.toStrin());
+             break;
+            } /*
             case 3: {
                 modificarObra mod = new modificarObra(null, null, null, null, n, n, n, null, null, n, null);
                 System.out.println("¿Qué obra quieres modificar?");
@@ -97,13 +105,10 @@ public class abrirMenu{
                 break;
             } 
             case 4: {
-
             }
             case 5: {
-
             }
             case 6: {
-
             }  
             case 7: {
                 System.out.println("Adios");
@@ -115,6 +120,7 @@ public class abrirMenu{
     }
     //sc.close();
 
-        }
+        
     }
+}
 
