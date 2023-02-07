25540ac15b34ce38215acfc01643ca949d962afb
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 06:22:32 +0100

    añado metodo pedirObra y modificar

--- src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
@@ -1,5 +1,41 @@
 package practicacom.practica.programacion.clasesMuseo;
+import java.util.Scanner;
 
-public class modificarObra extends verObras {
+import javax.lang.model.type.NoType;
+
+import practicacom.practica.programacion.clasesMuseo.abrirMenu;
+public class modificarObra extends obras {
+
+    Scanner sc = new Scanner(System.in);
+    String Ced;
+    Scanner dd = new Scanner(System.in);
+    
+    public modificarObra() {
+    }
+
+    public String getCed() {
+        return Ced;
+    }
+
+    public void setCed(String ced) {
+        Ced = ced;
+    }
+
+    public void pedirObra() {
+        System.out.print("¿Qué obra quieres visualizar?");
+        setCed(dd.nextLine());
+    }
+
+    public void modificar(obras aux) {
+        String nuevo;               
+            if (aux != null) {
+                System.out.print ("Cambios: ");
+                nuevo = dd.nextLine();
+                aux.setID(nuevo);
+            } 
+    }
+
+    
+    
     
 }
