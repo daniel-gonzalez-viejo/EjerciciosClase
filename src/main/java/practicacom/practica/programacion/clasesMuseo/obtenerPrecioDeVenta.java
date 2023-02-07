e98cc53cbecc2d2eff3de4e274ee47d23dedc23d
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 21:32:35 +0100

    añado metodo obtenerPrecio

--- src/main/java/practicacom/practica/programacion/clasesMuseo/obtenerPrecioDeVenta.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/obtenerPrecioDeVenta.java
@@ -2,4 +2,22 @@ package practicacom.practica.programacion.clasesMuseo;
 
 public class obtenerPrecioDeVenta {
     
+    public void obtenerPrecio(obras aux) {
+        if (aux != null) {
+            double precioFinal;
+            double comision = aux.getPrecio()*0.25;
+            System.out.println("");
+            System.out.println("------------------------------");
+            System.out.println("Nombre: " + aux.getNombre());
+            System.out.println("Altura(m): " + aux.getAltura());
+            System.out.println("Peso(t): " + aux.getPeso());
+            System.out.println("Precio(euros): " + aux.getPrecio());
+            System.out.println("Comisión de la galería: " + comision);
+            precioFinal = aux.getPrecio() + comision;
+            System.out.println("Precio total: " + precioFinal);
+            System.out.println("");
+        } 
+
+    }
+
 }
