a2ebbb5b4ec4b127216e02d17fa7baf47c474e34
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 18:26:31 +0100

    añado opciones al metodo modificar

--- src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
@@ -27,11 +27,46 @@ public class modificarObra extends obras {
     }
 
     public void modificar(obras aux) {
-        String nuevo;               
+        String nuevo;  
+        double nuevo2;
+        int nuevo3;             
             if (aux != null) {
-                System.out.print ("Cambios: ");
+                System.out.print ("ID: ");
                 nuevo = dd.nextLine();
                 aux.setID(nuevo);
+                System.out.print ("Tipo: ");
+                nuevo = dd.nextLine();
+                aux.setTipo(nuevo); //////
+                System.out.print ("Nombre: ");
+                nuevo = dd.nextLine();
+                aux.setNombre(nuevo);
+                System.out.print ("Autor: ");
+                nuevo = dd.nextLine();
+                aux.setAutor(nuevo);
+                System.out.print ("Precio: ");
+                nuevo2 = dd.nextDouble();
+                aux.setPrecio(nuevo2);
+                System.out.print ("Altura: ");
+                nuevo2 = dd.nextDouble();
+                aux.setAltura(nuevo2);
+                Scanner dd = new Scanner(System.in); //Esta inserción de escáner es para arreglar un error con el escáner
+                System.out.print ("Peso: ");
+                nuevo2 = dd.nextDouble();
+                aux.setPeso(nuevo2);
+                dd.nextLine(); //Esta inserción de escáner es para arreglar un error con el escáner
+                System.out.print ("Material: ");
+                nuevo = dd.nextLine();
+                aux.setMaterial(nuevo);
+                System.out.print ("Técnica: ");
+                nuevo = dd.nextLine();
+                aux.setTécnica(nuevo);
+                System.out.print ("Piezas: ");
+                nuevo3 = dd.nextInt();
+                aux.setPiezas(nuevo3);
+                dd.nextLine(); //Esta inserción de escáner es para arreglar un error con el escáner
+                System.out.print ("Descripción: ");
+                nuevo = dd.nextLine();
+                aux.setDescripción(nuevo);
             } 
     }
 
