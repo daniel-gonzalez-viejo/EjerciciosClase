1bcf51e9e8227802b4477e939e64e6d313390f8a
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 19:58:59 +0100

    Se crea y se añaden valores al metodo mostrarDatos

--- src/main/java/practicacom/practica/programacion/clasesMuseo/visualizarDatos.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/visualizarDatos.java
@@ -1,9 +1,14 @@
 package practicacom.practica.programacion.clasesMuseo;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import java.io.ObjectInputStream.GetField;
 
 =======
 >>>>>>> 495a01b (creo la clase visualizarDatos)
+=======
+import java.io.ObjectInputStream.GetField;
+
+>>>>>>> 00ccc2d (Se crea y se añaden valores al metodo mostrarDatos)
 
 public class visualizarDatos extends modificarObra {
 
@@ -33,15 +38,22 @@ public class visualizarDatos extends modificarObra {
 >>>>>>> 495a01b (creo la clase visualizarDatos)
 =======
 
-    @Override
-    public void pedirObra() {
-        super.pedirObra();
-    }
-
     public void mostrarDatos(obras aux) {
-        if(aux!= null) {
-            System.out.println("Nombre: " + getNombre());
-        }
+        String nuevo;  
+        double nuevo2;
+        int nuevo3;             
+            if (aux != null) {
+                System.out.println("");
+                System.out.println("------------------------------");
+                System.out.println("Nombre: " + aux.getNombre());
+                System.out.println("Autor: " + aux.getAutor());
+                System.out.println("Precio(euros): " + aux.getPrecio());
+                System.out.println("Altura(m): " + aux.getAltura());
+                System.out.println("Peso(t): " + aux.getPeso());
+                System.out.println("Número de piezas: " + aux.getPiezas());
+                System.out.println("Descripción: " + aux.getDescripción());
+                System.out.println("");
+            } 
     }
 >>>>>>> d7f84eb (añado metodo mostrarDatos)
     
