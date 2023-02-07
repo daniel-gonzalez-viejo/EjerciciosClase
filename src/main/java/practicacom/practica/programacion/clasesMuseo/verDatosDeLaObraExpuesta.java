737611866ff478f6265b6469211608de0e500889
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 21:00:37 +0100

    añado nueva clase para subsanar el error VerDatosDeLaObraExpuesta

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/verDatosDeLaObraExpuesta.java
@@ -0,0 +1,23 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class verDatosDeLaObraExpuesta extends modificarObra {
+
+    public verDatosDeLaObraExpuesta() {
+    }
+
+    public void mostrarDatos(obras aux) {         
+            if (aux != null) {
+                System.out.println("");
+                System.out.println("------------------------------");
+                System.out.println("Nombre: " + aux.getNombre());
+                System.out.println("Autor: " + aux.getAutor());
+                System.out.println("Precio(euros): " + aux.getPrecio());
+                System.out.println("Altura(m): " + aux.getAltura());
+                System.out.println("Peso(t): " + aux.getPeso());
+                System.out.println("Número de piezas: " + aux.getPiezas());
+                System.out.println("Descripción: " + aux.getDescripción());
+                System.out.println("");
+            } 
+    }
+
+}
