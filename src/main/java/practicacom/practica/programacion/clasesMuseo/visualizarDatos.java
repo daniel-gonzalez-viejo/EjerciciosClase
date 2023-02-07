00ccc2dd6eea553ad9409d7129679945fca73a8c
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 19:58:59 +0100

    Se crea y se añaden valores al metodo mostrarDatos

--- src/main/java/practicacom/practica/programacion/clasesMuseo/visualizarDatos.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/visualizarDatos.java
@@ -1,19 +1,28 @@
 package practicacom.practica.programacion.clasesMuseo;
+import java.io.ObjectInputStream.GetField;
+
 
 public class visualizarDatos extends modificarObra {
 
     public visualizarDatos() {
     }
 
-    @Override
-    public void pedirObra() {
-        super.pedirObra();
-    }
-
     public void mostrarDatos(obras aux) {
-        if(aux!= null) {
-            System.out.println("Nombre: " + getNombre());
-        }
+        String nuevo;  
+        double nuevo2;
+        int nuevo3;             
+            if (aux != null) {
+                System.out.println("");
+                System.out.println("------------------------------");
+                System.out.println("Nombre: " + aux.getNombre());
+                System.out.println("Autor: " + aux.getAutor());
+                System.out.println("Precio(euros): " + aux.getPrecio());
+                System.out.println("Altura(m): " + aux.getAltura());
+                System.out.println("Peso(t): " + aux.getPeso());
+                System.out.println("Número de piezas: " + aux.getPiezas());
+                System.out.println("Descripción: " + aux.getDescripción());
+                System.out.println("");
+            } 
     }
     
     
