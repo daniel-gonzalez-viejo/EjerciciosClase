8411e19a02e9cc66ca6d1ab92c8417af1c6dd5b4
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-07 04:02:26 +0100

    se han limpiado variables reiteradas en la clase obras

--- src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -2,130 +2,16 @@ package practicacom.practica.programacion.clasesMuseo;
 
 import java.util.Scanner;
 
-import javax.swing.text.html.HTMLDocument.RunElement;
+public class darDeAltaUnaObraDeArte extends obras{
 
-import practicacom.practica.programacion.clasesMuseo.obras;
-
-public class darDeAltaUnaObraDeArte{
-    private String ID;
-    private String Tipo;
-    private String Nombre;
-    private String Autor;
-    private double Precio;
-    private double Altura;
-    private double Peso;
-    private String Material;
-    private String Tecnica;
-    private int Piezas;
-    private String Descripción;
 
     Scanner sc = new Scanner(System.in);
 
     public darDeAltaUnaObraDeArte(String iD, String tipo, String nombre, String autor, double precio, double altura,
-            double peso, String material, String tecnica, int piezas, String descripción) {
-        ID = iD;
-        Tipo = tipo;
-        Nombre = nombre;
-        Autor = autor;
-        Precio = precio;
-        Altura = altura;
-        Peso = peso;
-        Material = material;
-        Tecnica = tecnica;
-        Piezas = piezas;
-        Descripción = descripción;
+            double peso, String material, String técnica, int piezas, String descripción) {
+        super(iD, tipo, nombre, autor, precio, altura, peso, material, técnica, piezas, descripción);
     }
 
-    public darDeAltaUnaObraDeArte() {
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String iD) {
-        ID = iD;
-    }
-
-    public String getTipo() {
-        return Tipo;
-    }
-
-    public void setTipo(String tipo) {
-        Tipo = tipo;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getAutor() {
-        return Autor;
-    }
-
-    public void setAutor(String autor) {
-        Autor = autor;
-    }
-
-    public double getPrecio() {
-        return Precio;
-    }
-
-    public void setPrecio(double precio) {
-        Precio = precio;
-    }
-
-    public double getAltura() {
-        return Altura;
-    }
-
-    public void setAltura(double altura) {
-        Altura = altura;
-    }
-
-    public double getPeso() {
-        return Peso;
-    }
-
-    public void setPeso(double peso) {
-        Peso = peso;
-    }
-
-    public String getMaterial() {
-        return Material;
-    }
-
-    public void setMaterial(String material) {
-        Material = material;
-    }
-
-    public String getTecnica() {
-        return Tecnica;
-    }
-
-    public void setTecnica(String tecnica) {
-        Tecnica = tecnica;
-    }
-
-    public int getPiezas() {
-        return Piezas;
-    }
-
-    public void setPiezas(int piezas) {
-        Piezas = piezas;
-    }
-
-    public String getDescripción() {
-        return Descripción;
-    }
-
-    public void setDescripción(String descripción) {
-        Descripción = descripción;
-    }
 
     public void darDeAlta() {
         System.out.print("ID: ");
@@ -140,23 +26,25 @@ public class darDeAltaUnaObraDeArte{
         setPrecio(sc.nextDouble());
         System.out.print("Altura: ");
         setAltura(sc.nextDouble());
+        Scanner sc = new Scanner(System.in); //Esta inserción de escáner es para arreglar un error con el escáner
         System.out.print("Peso: ");
-        setAltura(sc.nextDouble());
+        setPeso(sc.nextDouble());
         sc.nextLine(); //Esta inserción de escáner es para arreglar un error con el escáner
         System.out.print("Material: ");
         setMaterial(sc.nextLine());
         System.out.print("Técnica: ");
-        setTecnica(sc.next());
+        setTécnica(sc.next());
         System.out.print("Piezas: ");
         setPiezas(sc.nextInt());
-        System.out.print("Descripción ");
+        sc.nextLine(); //Esta inserción de escáner es para arreglar un error con el escáner
+        System.out.print("Descripción: ");
         setDescripción(sc.nextLine());
     }
 
     public obras nuevaObra() {
         darDeAlta();
         obras P6 = new obras(getID() , getTipo(), getNombre(), getAutor() , getPrecio() , getAltura() , getPeso() , 
-          getMaterial() , getTecnica() , getPiezas() , getDescripción());
+          getMaterial() , getTécnica() , getPiezas() , getDescripción());
         return P6;
     }
 
