41a92f29f0512ea8a93480a25c241f00ba781816
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-09 23:47:15 +0100

    factorizo metodo modificar

--- src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/modificarObra.java
@@ -22,10 +22,7 @@ public class modificarObra extends darDeAltaUnaObraDeArte {
         setCódigo(sc.nextLine());
     }
 
-    public void modificar(obras aux) {
-        String auxiliar_texto;  
-        double auxiliar_dinero;
-        int auxiliar_cifras;             
+    public void modificar(obras aux) {             
             if (aux != null) {
                 aux.setID(insertarID(TAMAÑO_MAXIMO_ID, MENSAJE_ERROR_ID));
                 aux.setTipo(insertarTipo(MENSAJE_ERROR_TIPO));
@@ -38,42 +35,6 @@ public class modificarObra extends darDeAltaUnaObraDeArte {
                 aux.setTécnica(insertarTecnica(MENSAJE_ERROR_TECNICA));
                 aux.setPiezas(insertarPiezas());
                 aux.setDescripción(insertarDescripción());
-
-
-
-
-
-
-
-
-
-                
-                auxiliar_texto = ;
-                aux.setNombre(auxiliar_texto);
-                System.out.print (AUTOR);
-                auxiliar_texto = insertarAutor();
-                aux.setAutor(auxiliar_texto);
-                System.out.print (PRECIO);
-                auxiliar_dinero = insertarPrecio();
-                aux.setPrecio(auxiliar_dinero);
-                System.out.print (ALTURA);
-                auxiliar_dinero = insertarAltura();
-                aux.setAltura(auxiliar_dinero);
-                System.out.print (PESO);
-                auxiliar_dinero = insertarPeso();
-                aux.setPeso(auxiliar_dinero);
-                System.out.print (MATERIAL);
-                auxiliar_texto = insertarMaterial(MENSAJE_ERROR_MATERIAL);
-                aux.setMaterial(auxiliar_texto);
-                System.out.print (TÉCNICA);
-                auxiliar_texto = insertarTecnica(MENSAJE_ERROR_TECNICA);
-                aux.setTécnica(auxiliar_texto);
-                System.out.print (PIEZAS);
-                auxiliar_cifras = insertarPiezas();
-                aux.setPiezas(auxiliar_cifras);
-                System.out.print (DESCRIPCIÓN);
-                auxiliar_texto = insertarDescripción();
-                aux.setDescripción(auxiliar_texto);
             }     
         }
 }
