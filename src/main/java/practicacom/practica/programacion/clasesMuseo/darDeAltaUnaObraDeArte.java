55da9e7cdeaf734caf25ce5e05a7ccd173597b33
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-09 22:21:16 +0100

    añado metodo insertarMaterial

--- src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -19,7 +19,7 @@ public class darDeAltaUnaObraDeArte extends verObras{
     final static String MENSAJE_ERROR_ID = "El ID es demasiado largo, por favor introduce un ID de máximo tres carácteres (por ejemplo 006): ";
     final static String MENSAJE_ERROR_TIPO = "Únicamente se aceptan obras de tipo [Pintura] o [Escultura]. Comprueba que esté escrito correctamente: ";
     final static String MENSAJE_ERROR_MATERIAL = "Únicamente se aceptan materiales de tipo [Acero], [Cobre] o [Hierro]. Comprueba que esté escrito correctamente: ";
-
+    final static String MENSAJE_ERROR_TECNICA = "Únicamente se aceptan técnicas de tipo [Óleo], [Acuarela] o [Carboncillo]. Comprueba que esté escrito correctamente: ";
 
     final static int TAMAÑO_MAXIMO_ID = 3;
     final static String OPCION_TIPO_PINTURA = "Pintura";
@@ -27,6 +27,9 @@ public class darDeAltaUnaObraDeArte extends verObras{
     final static String OPCION_MATERIAL_ACERO = "Acero";
     final static String OPCION_MATERIAL_COBRE = "Cobre";
     final static String OPCION_MATERIAL_HIERRO = "Hierro";
+    final static String OPCION_TECNICA_OLEO = "Oleo";
+    final static String OPCION_TECNICA_ACUARELA = "Acuarela";
+    final static String OPCION_TECNICA_CARBONCILLO = "Carboncillo";
 
 
     static boolean inserción_exitosa = false;    
@@ -118,6 +121,28 @@ public class darDeAltaUnaObraDeArte extends verObras{
         }
     }
 
+    public void insertarTecnica(String mensaje_error) {
+        if (getTipo().equals(OPCION_TIPO_ESCULTURA)) {
+            setTécnica("   ");
+        }
+        else if (getTipo().equals(OPCION_TIPO_PINTURA)) {
+            while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(TÉCNICA);
+            setTécnica(sc.nextLine());
+                if (getTécnica().equals(OPCION_TECNICA_ACUARELA) || getTécnica().equals(OPCION_TECNICA_CARBONCILLO) 
+                    || getTécnica().equals(OPCION_TECNICA_OLEO)) {
+                    inserción_exitosa = true;
+                    break;
+                }
+                else {
+                    System.out.println(mensaje_error);
+                }
+            }
+            inserción_exitosa = false;
+        }
+    }
+
 
 
     public void darDeAlta() {
@@ -129,7 +154,7 @@ public class darDeAltaUnaObraDeArte extends verObras{
         insertarAltura();
         insertarPeso();
         insertarMaterial(MENSAJE_ERROR_MATERIAL);
-        
+        insertarTecnica(MENSAJE_ERROR_TECNICA);
         /*System.out.print(TÉCNICA);
         setTécnica(sc.next());
         System.out.print(PIEZAS);
