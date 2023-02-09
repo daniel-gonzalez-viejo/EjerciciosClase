433ff6d08cdc1323d6a2adb912cac3cc21a6cdc3
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-09 21:56:08 +0100

    añado metodo insertarMaterial

--- src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/darDeAltaUnaObraDeArte.java
@@ -4,24 +4,30 @@ import java.io.ObjectInputStream.GetField;
 import java.util.Scanner;
 
 public class darDeAltaUnaObraDeArte extends verObras{
-    final String ID = "ID: ";
-    final String TIPO = "Tipo: ";
-    final String NOMBRE = "Nombre: ";
-    final String AUTOR = "Autor: ";
-    final String PRECIO = "Precio: ";
-    final String ALTURA = "Altura: ";
-    final String PESO = "Peso: ";
-    final String MATERIAL = "Material: ";
-    final String TÉCNICA = "Técnica: ";
-    final String PIEZAS = "Piezas: ";
-    final String DESCRIPCIÓN = "Descripción: ";
+    final static String ID = "ID: ";
+    final static String TIPO = "Tipo: ";
+    final static String NOMBRE = "Nombre: ";
+    final static String AUTOR = "Autor: ";
+    final static String PRECIO = "Precio: ";
+    final static String ALTURA = "Altura: ";
+    final static String PESO = "Peso: ";
+    final static String MATERIAL = "Material: ";
+    final static String TÉCNICA = "Técnica: ";
+    final static String PIEZAS = "Piezas: ";
+    final static String DESCRIPCIÓN = "Descripción: ";
 
-    final String MENSAJE_ERROR_ID = "El ID es demasiado largo, por favor introduce un ID de máximo tres carácteres (por ejemplo 006): ";
-    final String MENSAJE_ERROR_TIPO = "Únicamente se aceptan obras de tipo [Pintura] o [Escultura]. Comprueba que esté escrito correctamente: ";
+    final static String MENSAJE_ERROR_ID = "El ID es demasiado largo, por favor introduce un ID de máximo tres carácteres (por ejemplo 006): ";
+    final static String MENSAJE_ERROR_TIPO = "Únicamente se aceptan obras de tipo [Pintura] o [Escultura]. Comprueba que esté escrito correctamente: ";
+    final static String MENSAJE_ERROR_MATERIAL = "Únicamente se aceptan materiales de tipo [Acero], [Cobre] o [Hierro]. Comprueba que esté escrito correctamente: ";
 
-    final int TAMAÑO_MAXIMO_ID = 3;
-    final String OPCION_TIPO_PINTURA = "Pintura";
-    final String OPCION_TIPO_ESCULTURA = "Escultura";
+
+    final static int TAMAÑO_MAXIMO_ID = 3;
+    final static String OPCION_TIPO_PINTURA = "Pintura";
+    final static String OPCION_TIPO_ESCULTURA = "Escultura";
+    final static String OPCION_MATERIAL_ACERO = "Acero";
+    final static String OPCION_MATERIAL_COBRE = "Cobre";
+    final static String OPCION_MATERIAL_HIERRO = "Hierro";
+
 
     static boolean inserción_exitosa = false;    
 
@@ -57,6 +63,7 @@ public class darDeAltaUnaObraDeArte extends verObras{
                 System.out.println(mensaje_error);
             }
         }
+        inserción_exitosa = false;
     }
 
     public void insertarNombre() {
@@ -89,6 +96,28 @@ public class darDeAltaUnaObraDeArte extends verObras{
         setPeso(sc.nextDouble());
     }
 
+    public void insertarMaterial(String mensaje_error) {
+        if (getTipo().equals(OPCION_TIPO_PINTURA)) {
+            setMaterial("   ");
+        }
+        else if (getTipo().equals(OPCION_TIPO_ESCULTURA)) {
+            while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(MATERIAL);
+            setMaterial(sc.nextLine());
+                if (getMaterial().equals(OPCION_MATERIAL_ACERO) || getMaterial().equals(OPCION_MATERIAL_COBRE) 
+                    || getMaterial().equals(OPCION_MATERIAL_HIERRO)) {
+                    inserción_exitosa = true;
+                    break;
+                }
+                else {
+                    System.out.println(mensaje_error);
+                }
+            }
+            inserción_exitosa = false;
+        }
+    }
+
 
 
     public void darDeAlta() {
@@ -98,12 +127,10 @@ public class darDeAltaUnaObraDeArte extends verObras{
         insertarAutor();
         insertarPrecio(); 
         insertarAltura();
-        //Scanner sc = new Scanner(System.in); //Esta inserción de escáner es para arreglar un error con el escáner
         insertarPeso();
-        /*sc.nextLine(); //Esta inserción de escáner es para arreglar un error con el escáner
-        System.out.print(MATERIAL);
-        setMaterial(sc.nextLine());
-        System.out.print(TÉCNICA);
+        insertarMaterial(MENSAJE_ERROR_MATERIAL);
+        
+        /*System.out.print(TÉCNICA);
         setTécnica(sc.next());
         System.out.print(PIEZAS);
         setPiezas(sc.nextInt());
