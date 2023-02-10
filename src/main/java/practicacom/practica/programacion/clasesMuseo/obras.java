5e37423481a08d2d34edd2ace8634f45b5f8957b
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-10 21:17:02 +0100

    se transfieren los metodos de inserción desde la clase darDeAlta a la clase obras

--- src/main/java/practicacom/practica/programacion/clasesMuseo/obras.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/obras.java
@@ -1,6 +1,9 @@
 package practicacom.practica.programacion.clasesMuseo;
 
-public class obras{
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class obras {
     public static final String ROJO = "\u001B[31m";
     public static final String VERDE = "\u001B[32m";
     public static final String AMARILLO = "\u001B[33m";
@@ -9,7 +12,43 @@ public class obras{
     public static final String CYAN = "\u001B[36m";
     public static final String BLANCO = "\u001B[37m";
 
-    private String ID;
+    final static String ID = VERDE + "ID: " + BLANCO;
+    final static String TIPO = VERDE + "Tipo: " + BLANCO;
+    final static String NOMBRE = VERDE + "Nombre: " + BLANCO;
+    final static String AUTOR = VERDE + "Autor: " + BLANCO;
+    final static String PRECIO = VERDE + "Precio: " + BLANCO;
+    final static String ALTURA = VERDE + "Altura: " + BLANCO;
+    final static String PESO = VERDE + "Peso: " + BLANCO;
+    final static String MATERIAL = VERDE + "Material: " + BLANCO;
+    final static String TÉCNICA = VERDE + "Técnica: " + BLANCO;
+    final static String PIEZAS = VERDE + "Piezas: " + BLANCO;
+    final static String DESCRIPCIÓN = VERDE + "Descripción: " + BLANCO;
+
+    final static String MENSAJE_ERROR_ID_BLANCO = ROJO + "El ID no puede estar en blanco" + BLANCO;
+    final static String MENSAJE_ERROR_ID = ROJO + "El ID es demasiado largo, por favor introduce un ID de máximo tres carácteres (por ejemplo 006): " + BLANCO;
+    final static String MENSAJE_ERROR_TIPO = ROJO + "Únicamente se aceptan obras de tipo [Pintura] o [Escultura]. Comprueba que esté escrito correctamente: " + BLANCO;
+    final static String MENSAJE_ERROR_MATERIAL = ROJO + "Únicamente se aceptan materiales de tipo [Acero], [Cobre] o [Hierro]. Comprueba que esté escrito correctamente: " + BLANCO;
+    final static String MENSAJE_ERROR_TECNICA = ROJO + "Únicamente se aceptan técnicas de tipo [Oleo], [Acuarela] o [Carboncillo]. Comprueba que esté escrito correctamente: " + BLANCO;
+    final static String MENSAJE_ERROR_PRECIO = ROJO + "El precio debe ser de carácter numérico: " + BLANCO;
+    final static String MENSAJE_ERROR_ALTURA = ROJO + "La altura debe ser de carácter numérico: " + BLANCO;
+    final static String MENSAJE_ERROR_PESO = ROJO + "El peso debe ser de carácter numérico: " + BLANCO;
+    final static String MENSAJE_ERROR_PIEZAS = ROJO + "La cantidad de piezas debe ser de carácter numérico: " + BLANCO;
+
+
+
+    final static int TAMAÑO_MAXIMO_ID = 3;
+    final static String OPCION_TIPO_PINTURA = "Pintura";
+    final static String OPCION_TIPO_ESCULTURA = "Escultura";
+    final static String OPCION_MATERIAL_ACERO = "Acero";
+    final static String OPCION_MATERIAL_COBRE = "Cobre";
+    final static String OPCION_MATERIAL_HIERRO = "Hierro";
+    final static String OPCION_TECNICA_OLEO = "Oleo";
+    final static String OPCION_TECNICA_ACUARELA = "Acuarela";
+    final static String OPCION_TECNICA_CARBONCILLO = "Carboncillo";
+
+    static boolean inserción_exitosa = false;    
+
+    private String id;
     private String Tipo;
     private String Nombre;
     private String Autor;
@@ -21,9 +60,9 @@ public class obras{
     private int Piezas;
     private String Descripción;
 
-    public obras(String iD, String tipo, String nombre, String autor, double precio, double altura, double peso,
+    public obras(String id, String tipo, String nombre, String autor, double precio, double altura, double peso,
             String material, String técnica, int piezas, String descripción) {
-        this.ID = iD;
+        this.id = id;
         this.Tipo = tipo;
         this.Nombre = nombre;
         this.Autor = autor;
@@ -40,16 +79,56 @@ public class obras{
     }
 
     public String getID() {
-        return ID;
+        return id;
     }
     public void setID(String iD) {
-        ID = iD;
+        id = iD;
     }
+
+    public String insertarID(int n, String mensaje_error) {
+        while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(ID);
+            setID(sc.nextLine());
+            if (getID().isBlank()) {
+                System.out.println(MENSAJE_ERROR_ID_BLANCO);
+            }
+            else if (getID().length() <= n && !getID().isBlank()) {
+                inserción_exitosa = true;
+                break;
+            }    
+            else if (getID().length() > n) {
+                System.out.println(mensaje_error);
+            }
+            else {
+                System.out.println(MENSAJE_ERROR_ID_BLANCO);
+            }
+        }
+        inserción_exitosa = false;
+        return getID();
+    }
+
     public String getTipo() {
         return Tipo;
     }
     public void setTipo(String tipo) {
         Tipo = tipo;
+    }
+    public String insertarTipo(String mensaje_error) {
+        while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(TIPO);
+            setTipo(sc.nextLine());
+            if (getTipo().equals(OPCION_TIPO_PINTURA) || getTipo().equals(OPCION_TIPO_ESCULTURA)) {
+                inserción_exitosa = true;
+                break;
+            }
+            else {
+                System.out.println(mensaje_error);
+            }
+        }
+        inserción_exitosa = false;
+        return getTipo();
     }
     public String getNombre() {
         return Nombre;
@@ -57,23 +136,71 @@ public class obras{
     public void setNombre(String nombre) {
         Nombre = nombre;
     }
+
+    public String insertarNombre() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(NOMBRE);
+        setNombre(sc.nextLine());
+        return getNombre();
+    }
+
     public String getAutor() {
         return Autor;
     }
     public void setAutor(String autor) {
         Autor = autor;
     }
+
+    public String insertarAutor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(AUTOR);
+        setAutor(sc.nextLine());
+        return getAutor();
+    }
+
     public double getPrecio() {
         return Precio;
     }
     public void setPrecio(double precio) {
         Precio = precio;
     }
+
+    public double insertarPrecio() {
+        while(!inserción_exitosa) {
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.print(PRECIO);
+                setPrecio(sc.nextDouble());
+                inserción_exitosa = true;
+            }
+            catch (InputMismatchException e) {
+                System.out.println(MENSAJE_ERROR_PRECIO);
+            }
+        }
+        inserción_exitosa = false;
+        return getPrecio();
+    }
+
     public double getAltura() {
         return Altura;
     }
     public void setAltura(double altura) {
         Altura = altura;
+    }
+    public double insertarAltura() {
+        while(!inserción_exitosa) {
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.print(ALTURA);
+                setAltura(sc.nextDouble());
+                inserción_exitosa = true;
+            }
+            catch (InputMismatchException e) {
+                System.out.println(MENSAJE_ERROR_ALTURA);
+            }
+        }
+        inserción_exitosa = false;
+        return getAltura();
     }
     public double getPeso() {
         return Peso;
@@ -81,29 +208,113 @@ public class obras{
     public void setPeso(double peso) {
         Peso = peso;
     }
+    public double insertarPeso() {
+        while(!inserción_exitosa) {
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.print(PESO);
+                setPeso(sc.nextDouble());
+                inserción_exitosa = true;
+            }
+            catch (InputMismatchException e) {
+                System.out.println(MENSAJE_ERROR_PESO);
+            }
+        }
+        inserción_exitosa = false;
+        return getPeso();
+    }
+
     public String getMaterial() {
         return Material;
     }
     public void setMaterial(String material) {
         Material = material;
     }
+    public String insertarMaterial(String mensaje_error) {
+        if (getTipo().equals(OPCION_TIPO_PINTURA)) {
+            setMaterial("   ");
+        }
+        else if (getTipo().equals(OPCION_TIPO_ESCULTURA)) {
+            while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(MATERIAL);
+            setMaterial(sc.nextLine());
+                if (getMaterial().equals(OPCION_MATERIAL_ACERO) || getMaterial().equals(OPCION_MATERIAL_COBRE) 
+                    || getMaterial().equals(OPCION_MATERIAL_HIERRO)) {
+                    inserción_exitosa = true;
+                    break;
+                }
+                else {
+                    System.out.println(mensaje_error);
+                }
+            }
+            inserción_exitosa = false;
+        }
+        return getMaterial();
+    }
+
     public String getTécnica() {
         return Técnica;
     }
     public void setTécnica(String técnica) {
         Técnica = técnica;
     }
+    public String insertarTecnica(String mensaje_error) {
+        if (getTipo().equals(OPCION_TIPO_ESCULTURA)) {
+            setTécnica("   ");
+        }
+        else if (getTipo().equals(OPCION_TIPO_PINTURA)) {
+            while (!inserción_exitosa) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(TÉCNICA);
+            setTécnica(sc.nextLine());
+                if (getTécnica().equals(OPCION_TECNICA_ACUARELA) || getTécnica().equals(OPCION_TECNICA_CARBONCILLO) 
+                    || getTécnica().equals(OPCION_TECNICA_OLEO)) {
+                    inserción_exitosa = true;
+                    break;
+                }
+                else {
+                    System.out.println(mensaje_error);
+                }
+            }
+            inserción_exitosa = false;
+        }
+        return getTécnica();
+    }
+
     public int getPiezas() {
         return Piezas;
     }
     public void setPiezas(int piezas) {
         Piezas = piezas;
     }
+    public int insertarPiezas() {
+        while(!inserción_exitosa) {
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.print(PIEZAS);
+                setPiezas(sc.nextInt());
+                inserción_exitosa = true;
+            }
+            catch (InputMismatchException e) {
+                System.out.println(MENSAJE_ERROR_PIEZAS);
+            }
+        }
+        inserción_exitosa = false;
+        return getPiezas();
+    }
+
     public String getDescripción() {
         return Descripción;
     }
     public void setDescripción(String descripción) {
         Descripción = descripción;
+    }
+    public String insertarDescripción() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(DESCRIPCIÓN);
+        setDescripción(sc.nextLine());
+        return getDescripción();
     }
 
     public String Cabeceros() {
@@ -126,7 +337,7 @@ public class obras{
     @Override
     public String toString() {
         String s = "";
-        s += ROJO + ID + "     " + BLANCO;
+        s += ROJO + id + "     " + BLANCO;
         s += Tipo + "     ";
         s += Nombre + "     ";
         s += Autor + "     ";
