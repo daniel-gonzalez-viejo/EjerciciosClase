d77427505588b9712d4645086aacebb1b7a75ae8
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-10 21:16:25 +0100

    se añaden cambios menores visuales

--- src/main/java/practicacom/practica/programacion/clasesMuseo/imprimirEtiqueta.java
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/imprimirEtiqueta.java
@@ -7,7 +7,6 @@ public class imprimirEtiqueta extends modificarObra{
 
     public void imprimirUnaEtiqueta(obras aux) {
         if (aux != null) {
-            System.out.println("------------------------------");
             System.out.println(NOMBRE + aux.getNombre());
             System.out.println(AUTOR + aux.getAutor());
             System.out.println(DESCRIPCIÓN + aux.getDescripción());
