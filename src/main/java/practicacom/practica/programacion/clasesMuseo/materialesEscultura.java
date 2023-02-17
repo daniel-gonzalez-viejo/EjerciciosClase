e90363477874a998ff34719a5d7b938d312ebfe7
daniel-gonzalez-viejo <daniel.gonzalezviejo@tiernoparla.es>
2023-02-17 21:20:26 +0100

    añado clase materialesEscultura con sus correspondientes getter

---
+++ src/main/java/practicacom/practica/programacion/clasesMuseo/materialesEscultura.java
@@ -0,0 +1,37 @@
+package practicacom.practica.programacion.clasesMuseo;
+
+public class materialesEscultura {
+    protected final static String ACERO = "Acero";
+    protected final static String COBRE = "Cobre";
+    protected final static String HIERRO = "Hierro";
+    
+    protected final static String MATERIALES[] = {ACERO, COBRE, HIERRO};
+
+    public static String getAcero() {
+        return ACERO;
+    }
+    public static String getCobre() {
+        return COBRE;
+    }
+    public static String getHierro() {
+        return HIERRO;
+    }
+    public String[] getMateriales() {
+        return MATERIALES;
+    }
+
+    interface TIPO_MATERIALES {
+        public static String getAcero() {
+            return ACERO;
+        }
+        public static String getCobre() {
+            return COBRE;
+        }
+        public static String getHierro() {
+            return HIERRO;
+        }
+        public static String[] getMateriales() {
+            return MATERIALES;
+        }
+    }
+}
